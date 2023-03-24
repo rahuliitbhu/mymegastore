ec7d368915bf482c1fd973027c9e2176b56c629f
Rahul Kumar <rahuliitbhu18@gmail.com>
2023-03-24 19:36:02 +0530

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -6,13 +6,13 @@ const layout=({children})=>{
         <>
         <Head>
             
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" ></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
         <link rel="stylesheet" href="/style.css"/>
         </Head>
         <NavBar />
          {children}
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js" async></script>
         </>
     )
 }
